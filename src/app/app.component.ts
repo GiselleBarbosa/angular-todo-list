630decb5dbe7f14838c6503041f537a5a396111b
GiselleBarbosa <g88barbosa@gmail.com>
2026-02-07 14:13:11 -0300

    aula 74: (todo list) - Criando o header da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,11 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { ToggleComponent } from './toogle/toggle.component';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatButtonModule, ToggleComponent],
+  imports: [HeaderComponent],
   templateUrl: 'app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnChanges {
-  value = false;
-  number = 0;
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['number']) {
-      console.info('Valor :', changes['number'].currentValue);
-    }
-  }
-
-  toggle() {
-    this.value = !this.value;
-  }
-
-  increaseValue() {
-    this.number += 1;
-  }
-
-  decreaseValue() {
-    if (this.number >= 1) {
-      this.number -= 1;
-    }
-    return;
-  }
-}
+export class AppComponent {}
