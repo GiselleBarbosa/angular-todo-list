8cc75f51cc374fa33c0533bdfb43220050d1db75
GiselleBarbosa <g88barbosa@gmail.com>
2026-02-08 15:42:46 -0300

    aula 75: criando service de todos com signal e local storage

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, signal } from '@angular/core';
+import { TodoDTO } from '../models/model/todo.model';
+import { TodoKeyLocalStorage } from '../models/enum/todoKeyLocalStorage';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoSignals {
+  public todosState = signal<TodoDTO[]>([]);
+
+  public updateTodos(todo: TodoDTO): void {
+    if (todo.title?.trim() && todo.description?.trim() && todo.id != null) {
+      this.todosState.update((todos) => [...todos, todo]);
+    }
+
+    this.saveTodosInLocalStorage();
+  }
+
+  public saveTodosInLocalStorage(): void {
+    const todos = this.todosState();
+
+    if (todos.length === 0) return;
+
+    localStorage.setItem(TodoKeyLocalStorage.TODO_LIST, JSON.stringify(todos));
+  }
+
+  public loadTodosFromLocalStorage(): void {
+    const stored = localStorage.getItem(TodoKeyLocalStorage.TODO_LIST);
+
+    if (!stored) return;
+
+    this.todosState.set(JSON.parse(stored));
+  }
+}
